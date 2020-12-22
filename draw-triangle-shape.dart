2be1e6ceda40452a55074ba2a8cc96fbb4431685
Uzair Leo <52172428+uzairleo@users.noreply.github.com>
2020-12-22 15:37:48 +0500

    alyf design practice

--- draw-triangle-shape.dart
+++ draw-triangle-shape.dart
@@ -42,73 +42,123 @@ painter:DrawTriangleShape()
   ////
   //// will convert this to custom painter after extracting it from clipper
   ////
-//   class ShopsClipper extends CustomClipper<Path> {
-//   @override
-//   Path getClip(Size size) {
-//     var path = Path();
-//     path.moveTo(10, 0);
-//     path.lineTo(101, 0);
-//     path.quadraticBezierTo(111, 0, 111, 10);
-//     path.lineTo(111, 85);
-//     path.quadraticBezierTo(111, 95, 105, 100);
-//     path.lineTo(10, 168);
-//     path.quadraticBezierTo(0, 173, 0, 153);
-//     path.lineTo(0, 10);
-//     path.quadraticBezierTo(0, 0, 10, 0);
-//     return path;
-//   }
 
-//   @override
-//   bool shouldReclip(CustomClipper<Path> oldClipper) {
-//     return false;
-//   }
-// }
+class TestCustomPainter extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.only(top: 80.0, left: 50, right: 26),
+          child: Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: CustomPaint(
+                  size: Size(60, 20),
+                  painter: ShopsClipper(),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: CustomPaint(
+                  size: Size(60, 20),
+                  painter: ServicesClipper(),
+                ),
+              ),
+              Padding(
+                  padding: const EdgeInsets.only(top: 40.0, left: 10),
+                  child: CustomPaint(
+                    size: Size(20, 60),
+                    painter: HealthCareClipper(),
+                  )),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
 
-// class ServicesClipper extends CustomClipper<Path> {
-//   @override
-//   Path getClip(Size size) {
-//     var path = Path();
-//     path.moveTo(149, 0);
-//     path.lineTo(240, 0);
-//     path.quadraticBezierTo(250, 0, 250, 10);
-//     path.lineTo(250, 153);
-//     path.quadraticBezierTo(250, 173, 240, 168);
-//     path.lineTo(145, 100);
-//     path.quadraticBezierTo(139, 95, 139, 85);
-//     path.lineTo(139, 10);
-//     path.quadraticBezierTo(139, 0, 149, 0);
-//     return path;
-//   }
+class ShopsClipper extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var path = Path();
+    path.moveTo(10, 0);
+    path.lineTo(101, 0);
+    path.quadraticBezierTo(111, 0, 111, 10);
+    path.lineTo(111, 85);
+    path.quadraticBezierTo(111, 95, 105, 100);
+    path.lineTo(10, 168);
+    path.quadraticBezierTo(0, 173, 0, 153);
+    path.lineTo(0, 10);
+    path.quadraticBezierTo(0, 0, 10, 0);
 
-//   @override
-//   bool shouldReclip(CustomClipper<Path> oldClipper) {
-//     return false;
-//   }
-// }
+    var paint = Paint()
+      ..color = Colors.blue
+      ..style = PaintingStyle.fill;
 
-// class HealthCareClipper extends CustomClipper<Path> {
-//   @override
-//   Path getClip(Size size) {
-//     var path = Path();
-//     path.moveTo(10, 195);
-//     path.lineTo(120, 118);
-//     path.quadraticBezierTo(125, 113, 130, 118);
-//     path.lineTo(240, 195);
-//     path.quadraticBezierTo(250, 200, 250, 210);
-//     path.lineTo(250, 240);
-//     path.quadraticBezierTo(250, 250, 240, 250);
-//     path.lineTo(10, 250);
-//     path.quadraticBezierTo(0, 250, 0, 240);
-//     path.lineTo(0, 210);
-//     path.quadraticBezierTo(0, 200, 10, 195);
-//     return path;
-//   }
+    canvas.drawPath(path, paint);
+  }
 
-//   @override
-//   bool shouldReclip(CustomClipper<Path> oldClipper) {
-//     return false;
-//   }
-// }
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    throw UnimplementedError();
+  }
+}
 
-  
-  
+class ServicesClipper extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var path = Path();
+    path.moveTo(149, 0);
+    path.lineTo(240, 0);
+    path.quadraticBezierTo(250, 0, 250, 10);
+    path.lineTo(250, 153);
+    path.quadraticBezierTo(250, 173, 240, 168);
+    path.lineTo(145, 100);
+    path.quadraticBezierTo(139, 95, 139, 85);
+    path.lineTo(139, 10);
+    path.quadraticBezierTo(139, 0, 149, 0);
+
+    var paint = Paint()
+      ..color = Colors.blue
+      ..style = PaintingStyle.fill;
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    throw UnimplementedError();
+  }
+}
+
+class HealthCareClipper extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var path = Path();
+    path.moveTo(10, 195);
+    path.lineTo(120, 118);
+    path.quadraticBezierTo(125, 113, 130, 118);
+    path.lineTo(240, 195);
+    path.quadraticBezierTo(250, 200, 250, 210);
+    path.lineTo(250, 240);
+    path.quadraticBezierTo(250, 250, 240, 250);
+    path.lineTo(10, 250);
+    path.quadraticBezierTo(0, 250, 0, 240);
+    path.lineTo(0, 210);
+    path.quadraticBezierTo(0, 200, 10, 195);
+
+    var paint = Paint()
+      ..color = Colors.blue
+      ..style = PaintingStyle.fill;
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    throw UnimplementedError();
+  }
+}
